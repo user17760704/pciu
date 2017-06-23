c80162705189091a3052ee80e1ea932ff171bf99
Ravi <Ravi@powercosts.com>
2017-06-23 13:06:52 -0500

    Refactor

--- JavaEssentialTraining/src/olive/OliveMainExtendingClasses.java
+++ JavaEssentialTraining/src/olive/OliveMainExtendingClasses.java
@@ -1,12 +1,14 @@
-package extendingClasses;
+package olive;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import olive.model.enums.*;
+import olive.model.enums.OliveColorEnums;
+import olive.model.enums.OliveModelEnums;
+import olive.model.enums.OliveNamesEnums;
+import olive.operations.OlivePress;
 
-
-public class ExtendingClasses
+public class OliveMainExtendingClasses
 {
 
 	public static void main(String[] args)
@@ -17,8 +19,14 @@ public class ExtendingClasses
 		olives.add(new OliveModelEnums (OliveNamesEnums.LIGURIAN, OliveColorEnums.GREEN, 4));
 		olives.add(new OliveModelEnums (OliveNamesEnums.UNKNOWN, OliveColorEnums.PURPLE, 5));
 		
+		OlivePress op = new OlivePress();
 		
-
+		double yield = op.getOil(olives);
+		
+		System.out.println("Yield: " + yield);
+		
+		
 	}
+
 
 }
--- JavaEssentialTraining/src/olive/operations/OlivePress.java
+++ JavaEssentialTraining/src/olive/operations/OlivePress.java
@@ -1,19 +1,45 @@
 package olive.operations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import olive.model.OliveModel;
+import olive.model.enums.OliveModelEnums;
 
 public class OlivePress
 {
-	public double getOil(List<OliveModel> oliveModels)
+	public double getOil(List<?> oliveModels)
 	{
-		double yeild = 0; 
-		for (OliveModel oliveModel : oliveModels)
+		Object object = oliveModels.get(0);
+		
+		double yeild = 0;
+		
+		if(object instanceof OliveModelEnums)
 		{
-			yeild += oliveModel.getYeild();
+			List<OliveModelEnums> olives = (List<OliveModelEnums>) oliveModels;
+						
+			for(OliveModelEnums olive: olives)
+			{
+				yeild += olive.getYeild();
+			}
 		}
 		
+		else if (object instanceof OliveModel)
+		{
+			List<OliveModel> olives = (List<OliveModel>) oliveModels;
+			
+			for(OliveModel olive: olives)
+			{
+				yeild += olive.getYeild();
+			}
+		}
+		
+//		double yeild = 0; 
+//		for (OliveModel oliveModel : oliveModels)
+//		{
+//			yeild += oliveModel.getYeild();
+//		}
+//		
 		return yeild;
 	}
 }
