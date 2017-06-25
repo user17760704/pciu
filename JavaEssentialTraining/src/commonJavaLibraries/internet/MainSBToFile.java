7d8fa7de8f0bcce51073b7657babce683339c826
user17760704 <c.ravi10@yahoo.com>
2017-06-24 19:40:27 -0500

    Internet ops

--- JavaEssentialTraining/src/commonJavaLibraries/internet/MainSBToFile.java
+++ JavaEssentialTraining/src/commonJavaLibraries/internet/MainSBToFile.java
@@ -2,6 +2,19 @@ package commonJavaLibraries.internet;
 
 import java.net.MalformedURLException;
 
+/**
+ * This is the main method that will execute the application that will access a feed
+ * and dump the contents into a specified file
+ * 
+ * Step 1: Access a URL
+ * Step 2: Read all the contents into a <b>StringBuilder</b> object
+ * Step 3: Call the static method that will accept a file path and the StringBuilder object
+ * Step 4: Write the contents of the StringBuilder object into the specified file
+ * 
+ * @author Ravi
+ *
+ */
+
 public class MainSBToFile
 {
 	private static final String FLOWERS_FEED = "http://services.hanselandpetal.com/feeds/flowers.xml";
