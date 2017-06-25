a37b421ff2ff173a82819ad24ccc6528a050c947
user17760704 <c.ravi10@yahoo.com>
2017-06-24 19:43:35 -0500

    Generated javadocs

--- JavaEssentialTraining/src/commonJavaLibraries/internet/MainSBToFile.java
+++ JavaEssentialTraining/src/commonJavaLibraries/internet/MainSBToFile.java
@@ -4,12 +4,12 @@ import java.net.MalformedURLException;
 
 /**
  * This is the main method that will execute the application that will access a feed
- * and dump the contents into a specified file
+ * and dump the contents into a specified file.</br>
  * 
- * Step 1: Access a URL
- * Step 2: Read all the contents into a <b>StringBuilder</b> object
- * Step 3: Call the static method that will accept a file path and the StringBuilder object
- * Step 4: Write the contents of the StringBuilder object into the specified file
+ * Step 1: Access a URL</br>
+ * Step 2: Read all the contents into a <b>StringBuilder</b> object</br>
+ * Step 3: Call the static method that will accept a file path and the StringBuilder object</br>
+ * Step 4: Write the contents of the StringBuilder object into the specified file</br>
  * 
  * @author Ravi
  *
